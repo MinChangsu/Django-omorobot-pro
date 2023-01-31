582e5e96b21c7e15e8b8a5eaa037a29310358b21
Changsu <mgs1527@icloud.com>
2023-01-31 15:39:46 +0900

    33

--- minipj/settings.py
+++ minipj/settings.py
@@ -125,9 +125,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field    
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRSF_TRUSTED_ORIGINS =["https://port-0-mini99-sop272gldjuvto6.gksl2.cloudtype.app"]
