c9f9bc333a85e6ea66f2fc9d80ba56e506f5fc5b
Changsu <mgs1527@icloud.com>
2023-01-31 15:42:29 +0900

    44

--- minipj/settings.py
+++ minipj/settings.py
@@ -128,7 +128,7 @@ USE_TZ = False
 STATIC_URL = "/static/"
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field    
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
